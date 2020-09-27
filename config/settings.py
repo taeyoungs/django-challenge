4a4745208a61ab0c705475f8bc8724d49446ef2e
taeyoungs <xoxodudwkd@gmail.com>
2020-09-27 17:20:18 +0900

    EB config update

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = "b#s*_o(3t3ai_k(c5po@h7a=nj5#vjkd3u7ckhnx@)mi=8fn67"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG"))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".elasticbeanstalk.com",
+]
 
 
 # Application definition
